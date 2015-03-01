ceb1510e30686d852def14101d723da26c551b0e
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-03-01 13:40:35 +0900

    [add] add a template of httpd.conf

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -14,3 +14,11 @@ service "httpd" do
   action [:enable, :start]
 end
 
+template "httpd.conf" do
+  path "/etc/httpd/conf/httpd.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[httpd]'
+end
+
